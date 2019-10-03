455a27c04fda963474967b8754e0a94c2b38cc4c
Autologic <hengyiyu2018@outlook.com>
2019-10-03 19:48:06 +0800

    UI adjustments

--- Headers/_utility.h
+++ Headers/_utility.h
@@ -2,4 +2,27 @@
 
 #include <QString>
 
+#include <windows.h>
+
+#include <QFontDatabase>
+
+#include <QDebug>
+
+#include <QSize>
+
+#include <QPoint>
+
+#include <QMouseEvent>
+
+#include <QFile>
+
+#include <QDesktopWidget>
+
 static const QString IMG_DIR = ":/images/Share/images/";
+
+static const QString FONT_DIR = ":/fonts/Share/fonts/";
+
+static const QString STYLESHEET_DIR = ":/styleSheets/Share/styleSheets/";
+
+static const QString COLOR_SPACE_GRAY = "#798186";
+
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -7,13 +7,16 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QPixmap pixmap(IMG_DIR+"SplashScreen.png");
-    QSplashScreen startUp(pixmap);
-    startUp.show();
-    a.processEvents();
 
+    QPixmap pixmap(IMG_DIR + QStringLiteral("SplashScreen.png"));
+    QSplashScreen startUp(pixmap, Qt::WindowStaysOnTopHint);
+    startUp.setFont(QFont(QStringLiteral("微软雅黑")));
+    startUp.show();
+    startUp.showMessage(QStringLiteral("装载模块"), Qt::AlignBottom | Qt::AlignHCenter, Qt::white);
+    a.processEvents();
+    startUp.showMessage(QStringLiteral("初始化主界面"), Qt::AlignHCenter | Qt::AlignBottom, Qt::white);
     mainWindow w;
     w.show();
-    //startUp.finish(&w);
+    startUp.finish(&w);
     return a.exec();
 }

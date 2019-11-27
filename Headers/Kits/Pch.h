e924487a54c7a6306419420d2e81733a512e7812
HengyiYu <hengyiyu2018@outlook.com>
2019-11-27 16:53:42 +0800

    Renamed Precompile Header.

---
+++ Headers/Kits/Pch.h
@@ -0,0 +1,10 @@
+#ifndef STABLE_H
+#define STABLE_H
+
+#include <QtCore>
+#include <QtGui>
+
+// The purpose of this header is to accelerate
+// the entire compiling speed of this project.
+
+#endif // STABLE_H
